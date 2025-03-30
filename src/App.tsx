446446d54223e698ff8533671afcc963a51fd507
Drew Showalter <10665739+one19@users.noreply.github.com>
2025-03-30 22:31:06 +1100

    🔧 make mdx provider work properly.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
+import { MDXProvider } from '@mdx-js/react';
 import Slide1 from './slides/testSlide.mdx';
 import Slide2 from './slides/testSlide2.mdx';
 import { Deck } from './Deck';
 import { mdxComponents } from './MDXComponents';
 
 const App = () => (
-  <>
+  <MDXProvider components={mdxComponents}>
     <Deck
       slides={[
-        <Slide1 components={mdxComponents} key="1" />,
-        <Slide2 components={mdxComponents} key="2" />,
+        <Slide1 key="1" />,
+        <Slide2 key="2" />,
+        // more slides to come!
+        // TODO: support [<asides>, syntax]
       ]}
     />
-  </>
+  </MDXProvider>
 );
 
 export default App;
